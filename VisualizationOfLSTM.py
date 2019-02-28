f4d32e2f64546c91f699f7ad807433d8f62af4b2
John Dahlberg <john.dahlberg@live.com>
2019-02-28 11:35:00 +0100

    Implemented visualization of LSTM. Prediction gets only one word at a time. Will be updated to get the same sequence format as fit

--- VisualizationOfLSTM.py
+++ VisualizationOfLSTM.py
@@ -1,7 +1,7 @@
 '''
 @author: John Henry Dahlberg
 
-2019-02-05
+2019-02-22
 '''
 
 from __future__ import print_function
@@ -18,7 +18,14 @@ import re
 import zipfile
 import lxml.etree
 from terminaltables import SingleTable
-
+import gensim
+import string
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers.recurrent import LSTM
+from keras.layers import Dense, Activation
+from keras.layers.embeddings import Embedding
+from keras.callbacks import LambdaCallback
 
 class VisualizeLSTM(object):
 
@@ -36,12 +43,18 @@ class VisualizeLSTM(object):
             mode = c_int(mode.value | 4)
             windll.kernel32.SetConsoleMode(c_int(stdout_handle), mode)
 
-    def loadVocabulary(self):
-        is_binary = self.model_file[-4:] == '.bin'
-        print('Loading model "' + self.model_file + '"...')
-        word2vec_model = KeyedVectors.load_word2vec_format(self.model_file, binary=is_binary)
-        K = size(word2vec_model.vectors, 1)
+        if self.word_domain:
+            self.word2vec_model, self.input_sequence, self.K = self.loadVocabulary()
+            weights_word_embedding = self.word2vec_model.wv.syn0
+            self.M = size(weights_word_embedding, 0)
+        else:
+            self.input_sequence, self.charToInd, self.indToChar = self.loadCharacters()
+            self.K = len(self.indToChar)
 
+        if self.nHiddenNeurons == 'Auto':
+            self.nHiddenNeurons = self.K
+
+    def loadVocabulary(self):
         words = []
 
         print('Loading text file "' + self.textFile + '"...')
@@ -51,6 +64,7 @@ class VisualizeLSTM(object):
             print('Extracting words...')
             input_text = '\n'.join(doc.xpath('//content/text()'))
             words.extend(re.findall(r"\w+|[^\w]", input_text))
+            sentences = list(self.evenlySplit(words, self.seqLength))
         else:
             with open(self.textFile, 'r') as f:
                 lines = f.readlines()
@@ -59,36 +73,171 @@ class VisualizeLSTM(object):
                     words.extend(re.findall(r"\w+|[^\w]", line))
                     words.append('\n')
 
-        return word2vec_model, words, K
+        if '.' in self.model_file:
+            is_binary = self.model_file[-4:] == '.bin'
+            print('Loading model "' + self.model_file + '"...')
+            word2vec_model = KeyedVectors.load_word2vec_format(self.model_file, binary=is_binary)
+            K = size(word2vec_model.vectors, 1)
+        else:
+            K = 300
+            sentences = [words]  # [''.join(words).split()]
+            word2vec_model = gensim.models.Word2Vec(sentences, size=K, min_count=1, window=5, iter=10, sg=0)
+
+        return word2vec_model, sentences, K
+
+    def evenlySplit(self, items, lengths):
+        for i in range(0, len(items)-lengths, lengths):
+            yield items[i:i + lengths]
+
+    def preProcessData(self):
+        print('Preprocessing data...')
+        x = zeros([self.seqLength, len(self.input_sequence)], dtype=int32)
+        y = zeros([len(self.input_sequence)], dtype=int32)
+
+        '''
+        input_sequence_indices = zeros([len(self.input_sequence)], dtype=int32)
+        for i, entity in enumerate(len(self.input_sequence)):
+            if i%int(len(self.input_sequence)/100):
+                print(str(int(i/len(input_sequence_indices))) + ' %')
+            try:
+                input_sequence_indices[i] = self.wordToIndex(entity)
+            except KeyError:
+                self.word2vec_model[entity] = random.uniform(-0.25, 0.25, self.K)
+                input_sequence_indices[i] = self.wordToIndex(entity)
+                print("Entity '" + entity + "'" + ' added to model.')
+        '''
+
+        for i, sentence in enumerate(self.input_sequence):
+            if i % int(len(self.input_sequence) / 100):
+                print(str(int(i / len(self.input_sequence))) + ' %')
+
+            for t, entity in enumerate(sentence[:-1]):
+                try:
+                    x[t, i] = self.wordToIndex(entity)
+                except KeyError:
+                    self.word2vec_model[entity] = random.uniform(-0.25, 0.25, self.K)
+                    x[t, i] = self.wordToIndex(entity)
+                    print("Entity '" + entity + "'" + ' added to model.')
+            label_entity = sentence[-1]
+            try:
+                y[i] = self.wordToIndex(label_entity)
+            except KeyError:
+                self.word2vec_model[label_entity] = random.uniform(-0.25, 0.25, self.K)
+                y[i] = self.wordToIndex(label_entity)
+                print("Entity '" + entity + "'" + ' added to model.')
+
+        return x, y
+
+    def on_epoch_end(self, _, logs={}):
+        self.losses.append(logs.get('loss'))
+
+    def trainLSTM(self):
+        print('\nTraining LSTM...')
+        if not self.model_init == 'Load':
+            self.lstm_model = Sequential()
+            self.lstm_model.add(Embedding(input_dim=self.M, output_dim=self.K, weights=[self.word2vec_model.wv.syn0]))
+            self.lstm_model.add(LSTM(units=self.K))
+            self.lstm_model.add(Dense(units=self.M))
+            self.lstm_model.add(Activation('softmax'))
+            self.lstm_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+        else:
+            self.lstm_model = load_model('LSTM/lstm_model.h5')
+
+        if self.word_domain:
+            self.domain_specification = 'Words'
+        else:
+            self.domain_specification = 'Characters'
+
+        self.lstm_model.summary()
+        self.losses = []
+        for e in range(0, len(self.input_sequence) - self.seqLength*self.batch_size - 1, self.seqLength*self.batch_size):
+            x, y = self.getWords(e)
+            self.lstm_model.fit(x, y, batch_size=128, epochs=1, callbacks=[LambdaCallback(on_epoch_end=self.on_epoch_end)])
+
+            if self.saveParameters:
+                self.lstm_model.save('LSTM/lstm_model.h5')
+
+            table, neuron_activation_map, inputs = self.synthesizeText()
+
+            with open('config.txt', 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line.split('#')[0]
+                    if 'plotColorMap:' in line:
+                        self.plotColorMap = ''.join(line.split()).split(':')[1] == 'True'
+                        break
+                    else:
+                        self.plotColorMap = False
+
+            if self.plotColorMap:
+                self.plotNeuralActivity(inputs, neuron_activation_map)
+
+            a = e%(self.seqLength*self.batch_size)
+            b = (self.seqLength*self.batch_size)
+            print('\nEpoch: ' + str(int(e/self.seqLength*self.batch_size)) + ', Epoch process: ' + str('{0:.2f}'.format(a/b*100) + '%'
+                  + ', Loss: ' + str('{0:.2f}'.format(self.losses[-1])) + ', Neuron of interest: ' +
+                  str(self.neuronsOfInterest) + '(/' + str(self.nHiddenNeurons) + ')'))
+
+            print(table)
 
     def getWords(self, e):
-        x_sequence = self.input_sequence[e:e + self.seqLength]
-        y_sequence = self.input_sequence[e + 1:e + self.seqLength + 1]
 
-        x = []
-        y = []
+        sentences = [self.input_sequence[e + i*self.seqLength:e + (i+1)*self.seqLength] for i in range(self.batch_size)]
 
-        for i in range(len(x_sequence)):
-            x_word = x_sequence[i]
-            y_word = y_sequence[i]
+        x = zeros((self.batch_size, self.seqLength), dtype=int32)
+        y = zeros((self.batch_size), dtype=int32)
+
+        for i, sentence in enumerate(sentences):
+            for t, entity in enumerate(sentence[:-1]):
+                try:
+                    x[i, t] = self.wordToIndex(entity)
+                except KeyError:
+                    self.word2vec_model[entity] = random.uniform(-0.25, 0.25, self.K)
+                    x[i, t] = self.wordToIndex(entity)
+                    print("Entity '" + entity + "'" + ' added to model.')
+
+            label_entity = sentence[-1]
+
             try:
-                x.append(array([self.word2vec_model[x_word]]).T)
+                y[i] = array([self.wordToIndex(label_entity)])
             except KeyError:
-                self.word2vec_model[x_word] = random.uniform(-0.25, 0.25, self.K)
-                x.append(array([self.word2vec_model[x_word]]).T)
-                print("Word '" + x_word + "'" + ' added to model.')
-            try:
-                y.append(array([self.word2vec_model[y_word]]).T)
-            except KeyError:
-                self.word2vec_model[y_word] = random.uniform(-0.25, 0.25, self.K)
-                y.append(array([self.word2vec_model[y_word]]).T)
-                # print("Word '" + y_word + "'" + ' added to model.')
-        return x_sequence, y_sequence, x, y
+                self.word2vec_model[label_entity] = random.uniform(-0.25, 0.25, self.K)
+                y[i] = array([self.wordToIndex(label_entity)])
+                print("Entity '" + entity + "'" + ' added to model.')
 
-    def synthesizeText(self, x0, hPrev, seqLength, weights={}):
-        if not weights:
-            weightsTuples = [(self.weights[i], getattr(self, self.weights[i])) for i in range(len(self.weights))]
-            weights = dict(weightsTuples)
+        return x, y
+
+    def generateWords(self):
+        
+        #sentences = [self.input_sequence[e + i*self.seqLength:e + (i+1)*self.seqLength] for i in range(self.batch_size)]
+
+        x = zeros((self.batch_size, self.seqLength), dtype=int32)
+        y = zeros((self.batch_size), dtype=int32)
+
+        # for i, sentence in enumerate(self.sentences[self.e:self.e+self.batch_size]):
+        for i, sentence in enumerate(self.sentences[self.e:self.e+self.batch_size]):
+            for t, entity in enumerate(sentence[:-1]):
+                try:
+                    x[i, t] = self.wordToIndex(entity)
+                except KeyError:
+                    self.word2vec_model[entity] = random.uniform(-0.25, 0.25, self.K)
+                    x[i, t] = self.wordToIndex(entity)
+                    print("Entity '" + entity + "'" + ' added to model.')
+
+            label_entity = sentence[-1]
+
+            try:
+                y[i] = array([self.wordToIndex(label_entity)])
+            except KeyError:
+                self.word2vec_model[label_entity] = random.uniform(-0.25, 0.25, self.K)
+                y[i] = array([self.wordToIndex(label_entity)])
+                print("Entity '" + entity + "'" + ' added to model.')
+
+        return x, y
+    
+    def synthesizeText(self):
+
+        seqLength = self.lengthSynthesizedText
 
         self.neuronsOfInterest = []
         self.neuronsOfInterestPlot = []
@@ -98,46 +247,52 @@ class VisualizeLSTM(object):
 
         table_data = [['Neuron ' + str(self.neuronsOfInterest[int(i/2)]), ''] if i % 2 == 0 else ['\n', '\n'] for i in range(2*len(self.neuronsOfInterest))]
         table = SingleTable(table_data)
-        table.table_data.insert(0, ['Neuron ', 'Predicted sentence from previous ' + self.domain_specification[:-1].lower() + ' "' + x0 + '"'])
+        table.table_data.insert(0, ['Neuron ', 'Predicted sentence '])
 
         max_width = table.column_max_width(1)
 
-        y_n = [[] for i in range(len(self.neuronsOfInterest))]
-        y = [[] for i in range(len(self.neuronsOfInterest))]
-
-        if self.word_domain:
-            x = [array([self.word2vec_model[x0]]).T]
-        else:
-            sample = copy(x0)
+        y_n = [[] for _ in range(len(self.neuronsOfInterest))]
+        y = [[] for _ in range(len(self.neuronsOfInterest))]
 
         neuron_activation_map = zeros((self.nHiddenNeurons, seqLength))
 
+        entity_indices = [self.wordToIndex(' ')]
+
         for t in range(seqLength):
-            if not self.word_domain:
-                x = self.seqToOneHot(sample)
-            output, h, a = self.forwardProp(x, hPrev, weights)
-            hPrev = deepcopy(h[-1])
 
-            neuron_activation_map[:, t] = a[-1][:, 0]
-            neuronActivations = a[-1][self.neuronsOfInterest]
+            import keras.backend as K
 
-            if self.word_domain:
-                output_word_vector = output[0][:, 0]
-                list_most_similar = self.word2vec_model.most_similar(positive=[output_word_vector], topn=200)
-                similarities = array([list_most_similar[i][1] for i in range(len(list_most_similar))])
-                p = similarities[similarities > 0]/sum(similarities[similarities > 0])
-            else:
-                p = output[-1]
+            output = self.lstm_model.predict(x=array(entity_indices))
 
+            aaoutput1 = self.lstm_model.predict(x=[3])
+            aaoutput2 = self.lstm_model.predict(x=array([range(4)]))
+            aaoutput22 = self.lstm_model.predict(x=array([0, 1, 2, 3]))
+            aaoutput3 = self.lstm_model.predict(x=array([3]))
+
+            # get_all_layer_outputs = K.function([self.lstm_model.layers[0].input],
+             #   [l.output for l in self.lstm_model.layers])
+            # all_layers = get_all_layer_outputs([atleast_2d(entity) for entity in entity_indices])
+
+            lstm_layer = K.function([self.lstm_model.layers[0].input], [self.lstm_model.layers[1].output])
+            # activations = lstm_layer([atleast_2d(entity) for entity in entity_indices])[0].T
+            activations = lstm_layer([atleast_2d(entity_indices[-1])])[0].T
+
+            #inference_model = Sequential()
+            #inference_model.add(Embedding(input_dim=self.M, output_dim=self.K, weights=self.lstm_model.layers[0].get_weights()))
+            #inference_model.add(LSTM(units=self.K, weights=self.lstm_model.layers[1].get_weights()))
+            #activations = inference_model.predict(x=entity_indices).T
+
+            neuron_activation_map[:, t] = activations[:, 0]
+            neuronActivations = activations[self.neuronsOfInterest]
+
+            p = self.softmax(output[-1])
             cp = cumsum(p)
             rand = random.uniform()
             diff = cp - rand
             sample_index = [i for i in range(len(diff)) if diff[i] > 0][0]
+            entity_indices.append(sample_index)
 
-            if self.word_domain:
-                sample = list_most_similar[sample_index][0]
-            else:
-                sample = self.indToChar.get(sample_index)
+            sample = self.indexToWord(sample_index)
 
             for i in range(len(self.neuronsOfInterest)):
 
@@ -201,6 +356,13 @@ class VisualizeLSTM(object):
         table.table_data[0][1] += color_range_str
 
         return table.table, neuron_activation_map, y_n[0]
+
+
+    def softmax(self, s):
+        exP = exp(s)
+        p = exP / exP.sum()
+
+        return p
 
     def plotNeuralActivity(self, inputs, neuron_activation_map):
         with open('FeaturesOfInterest.txt', 'r') as f:
@@ -319,26 +481,30 @@ class VisualizeLSTM(object):
                                 self.interval_limits.append(int(interval))
                         self.interval_limits = array(self.interval_limits)
 
+    def wordToIndex(self, word):
+        return self.word2vec_model.wv.vocab[word].index
+
+    def indexToWord(self, index):
+        return self.word2vec_model.wv.index2word[index]
 
 def main():
 
     attributes = {
-        'textFile': 'Data/ted_en.zip',  # Name of book text file, needs to be longer than lengthSynthesizedTextBest
-        'model_file': 'Data/glove_short.txt',  # 'Data/glove_840B_300d.txt',  #
-        'word_domain': False,  # True for words, False for characters
-        'weightInit': 'Load',  # 'He', 'Load' or 'Random'
+        'textFile': 'LordOfTheRings2.txt', # "'Data/ted_en.zip',  # Name of book text file, needs to be longer than lengthSynthesizedTextBest
+        'model_file': 'None', #''Data/glove_840B_300d.txt',  # 'Data/glove_short.txt',  #
+        'word_domain': True,  # True for words, False for characters
+        'model_init': 'Load',  # 'Load' or 'Random'
         'nHiddenNeurons': 'Auto',  # Number of hidden neurons
+        'batch_size': 2,
         'nEpochs': 100,  # Total number of epochs, each corresponds to (n book characters)/(seqLength) seq iterations
-        'seqLength': 25,  # Sequence length of each sequence iteration
-        'lengthSynthesizedText': 200,  # Sequence length of each print of text evolution
-        'lengthSynthesizedTextBest': 1000,  # Sequence length of final best sequence, requires saveParameters
+        'seqLength': 5,  # Sequence length of each sequence iteration
+        'lengthSynthesizedText': 10,  # Sequence length of each print of text evolution
+        'lengthSynthesizedTextBest': 100,  # Sequence length of final best sequence, requires saveParameters
         'saveParameters': False  # Save best weights with corresponding arrays iterations and smooth loss
     }
 
-    if not attributes['adaGradSGD']:
-        attributes['eta'] = 0.01*attributes['eta']
-
     lstm_vis = VisualizeLSTM(attributes)
+    lstm_vis.trainLSTM()
 
 
 if __name__ == '__main__':
